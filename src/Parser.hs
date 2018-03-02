af8721b2a31a56506084be20f02713016e7170cd
Sophia Gold <sophiagoldnyc@gmail.com>
2018-03-02 02:10:05 -0500

    benchmarks suite

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,37 +3,45 @@
 module Parser where
 
 import           Parser.Types
-import           Control.Lens.Tuple
+import           Control.Lens.Each (each)
+import           Control.Lens.Operators
+import           Control.Lens.Traversal (partsOf)
+import           Prelude hiding (take)
 import           Data.Attoparsec (parseOnly, Parser)
 import           Data.Attoparsec.ByteString.Char8
+import           Data.Bifunctor (second)
+import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import           Data.Char (digitToInt)
-import           Data.List (chunksOf)
+import           Data.Int (Int32)
 import           Data.Time.Clock (picosecondsToDiffTime)
 
-parsePkt :: BS.ByteString -> Either String (AcceptTime, Packet)
-parsePkt bs = let p = skipMany1 "B6034"
-                   *> parseICode
-                   *> skipMany1 $ take 12
-                   *> parseBids
-                   *> skipMany1 $ take 7
-                   *> parseAsks
-                   *> skipMany1 $ take 50
-                   *> parseAcceptTime
-              in case parseOnly p bs of
-                   Left err       -> Left err
-                   Right i b a at -> Right (at, Packet i b a)
+parsePkt :: ByteString -> Either String (AcceptTime, Packet)
+parsePkt bs = case parse (skipMany1 "B6034" *> parseICode) bs of
+  Fail _ _ _  -> Left "Failed parsing issue code."
+  Partial _   -> Left "Failed parsing issue code." 
+  Done rest i -> case parse (take 7 *> parseBids) rest of
+    Fail _ _ _  -> Left "Failed parsing bid quotes."
+    Partial _   -> Left "Failed parsing bid quotes."
+    Done rest b -> case parse (take 7 *> parseAsks) rest of
+      Fail _ _ _  -> Left "Failed parsing ask quotes."
+      Partial _   -> Left "Failed parsing ask quotes."
+      Done rest a -> case parse (take 50 *> parseAcceptTime) rest of
+        Fail _ _ _  -> Left "Failed parsing accept time."
+        Partial _   -> Left "Failed parsing accept time."
+        Done _ at   -> Right (at, Packet i b a)
 
-parseICode :: BS.ByteString -> Parser IssueCode
-parseICode = \bs -> take 12
-  -- case parse $ take 12 of
-  --   Fail bs ctxs msg -> putStrLn msg
-  --   Partial cont     -> putStrLn "Failed parsing issue code."
-  --   Done bs result   -> result
+parseICode :: ByteString -> Either String (ByteString, IssueCode)
+parseICode = \bs ->
+  case parseOnly $ take 12 of
+    Done rest result -> (rest, result)
+    _                -> "Failed parsing issue code." 
 
-parseAcceptTime :: BS.ByteString -> Parser AcceptTime
-parseAcceptTime = \bs -> let p = (toInteger . digitToInt) <$> chunksOf 2 $ take 8
-                             toPico = \t -> zipWith3 (\a b x -> a^b * c) t [6, 12, 12, 12] [1, 1, 60, 3600]
+parseAcceptTime :: ByteString -> Parser AcceptTime
+parseAcceptTime = \bs -> let p = (toInteger . digitToInt) <$> chunk2 $ take 8 where
+                               chunk2 [] = []
+                               chunk2 l = take 2 l : (chunk2 $ drop 2 l)
+                             toPico = \t -> zipWith3 (\a b c -> a^b * c) t [6, 12, 12, 12] [1, 1, 60, 3600]
                              toTime = \t -> picosecondsToDiffTime $ sum $ toPico t
                          in second toTime p
   -- case parse p of
@@ -41,23 +49,28 @@ parseAcceptTime = \bs -> let p = (toInteger . digitToInt) <$> chunksOf 2 $ take 
   --   Partial cont     -> putStrLn "Failed parsing issue code."
   --   Done bs result   -> picosecondsToDiffTime $ sum $ toPico p
       
-parseBids :: BS.ByteString -> Either String Bids
-parseBids bs = go p 0 where
-  go p 12 = case parse p bs of
-              Fail bs ctxs msg -> Left msg
-              Partial cont     -> Left "Failed parsing bid."
-              Done bs result   -> let b = foldr (\x ~(xs,ys) -> (x:ys,xs)) ([],[]) $ digitToInt <$> result
-                                  in Right $ Bids (BidQuants <$> snd b) (BidPrices <$> fst b)
-  go p i  = case even i of
-              True  -> p (p *> take 5) (i + 1)
-              False -> p (p *> take 7) (i + 1)
+parseBids :: ByteString -> Either String Bids
+parseBids bs = pEven 0 where
+  pEven 12 = case parse pg Even bs of
+               Fail bs ctxs msg -> Left msg
+               Partial cont     -> Left "Failed parsing bid."
+               Done bs result   -> let b = splitAt 6 $ digitToInt result
+                                   in Right ((undefined & partsOf each
+                                              .~ (snd result) :: (Int32,Int32,Int32,Int32))
+                                            , (undefined & partsOf each
+                                               .~ (fst result) :: (Int32,Int32,Int32,Int32)))
+  pEven i = (\bs -> take 5 bs) (i + 1) 
+  pPOdd i = (\bs -> take 7 bs) (i + 1) 
     
-parseAsks :: BS.ByteString -> Either String Asks
-parseAsks bs = go p 0 where
-  go p 12 = case parse p bs of
-              Fail bs ctxs msg -> Left msg
-              Partial cont     -> Left "Failed parsing bid."
-              Done bs result   -> Right $ Asks (AskPrices <$> result) (AskQuants <$> result)
-  go p i  = case even i of
-              True  -> p (p *> take 5) (i + 1)
-              False -> p (p *> take 7) (i + 1)
+parseAsks :: ByteString -> Either String Asks
+parseAsks bs = pEven 0 where
+  pEven 12 = case parse pEven bs of
+               Fail bs ctxs msg -> Left msg
+               Partial cont     -> Left "Failed parsing bid."
+               Done bs result   -> let a = splitAt 6 $ digitToInt resultRight
+                                   in Right ((undefined & partsOf each
+                                              .~ (snd result) :: (Int32,Int32,Int32,Int32))
+                                            , (undefined & partsOf each
+                                               .~ (fst result) :: (Int32,Int32,Int32,Int32)))
+  pEven i = (\bs -> take 5 bs) *> pOdd (i + 1)
+  pOdd i  = (\bs -> take 7 bs) *> pEven (i + 1)
--- test/Bench.hs
+++ test/Bench.hs
@@ -1,24 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Test where
+module Bench where
 
 import           Main
 import           Capture
 import           Parser
 import           Control.Monad.State.Lazy (execState)
+import           Criterion (bench, whnf)
 
-quoteAcceptOrdTest :: IO ()
-quoteAcceptOrdTest = do
+quoteAcceptOrdTime :: String
+quoteAcceptOrdTime = do
   buf <- readPkts "mdf-kospi200.20110216-0.pcap" 
-  (putStrLn . show) <$> execState buf
+  show $ bench "accept time order" (whnf $ execState buf)
 
-quotePktOrderTest :: IO ()
-quotePktOrderTest = do
+quotePktOrder :: String
+quotePktOrder = do
   buf <- readPkts "mdf-kospi200.20110216-0.pcap"
-  (putStrLn . show) <$> execState buf
+  show $ bench "packet order" (whnf $ execState buf)
 
-parsePktTest :: IO ()
-parsePktTest = do
+parsePktTime :: String
+parsePktTime = do
   let input = ("B6034KR4301F3250500940000679900096000030800095000009400094000023100093000019900092000013400077890009700002340009800001300009900002820010000004150010100000520039700120007000800160009004590011001400170027000709002997"
                :: C.ByteString)
-  putStrLn $ show $ parsePkt input
+  show $ bench "parser" (whnf $ parsePkt input)
