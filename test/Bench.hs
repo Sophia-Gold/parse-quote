ddc7c5e75b81c15f197fd740f18712c705eb1409
Sophia Gold <sophiagoldnyc@gmail.com>
2018-03-04 12:57:26 -0500

    s/test-framework/tasty

--- test/Bench.hs
+++ test/Bench.hs
@@ -5,11 +5,11 @@ module Bench where
 import           Capture
 import           Parser
 import           Control.Concurrent.MVar
-import           Criterion (bench, whnf)
+import           Criterion (Benchmark, bench, whnf)
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.Map.Strict as Map (empty)
 import qualified Data.Sequence as S (empty)
-import           Test.Framework (defaultMain)
+import           Test.Tasty (defaultMain)
 
 main :: IO ()
 main = defaultMain
@@ -18,22 +18,22 @@ main = defaultMain
   , parsePktTime
   ]
 
-quoteAcceptOrdTime :: IO String
+quoteAcceptOrdTime :: Benchmark
 quoteAcceptOrdTime = do
   acceptOrdBuf <- newMVar Map.empty
   readPkts "mdf-kospi200.20110216-0.pcap" (enqueueAcceptOrd acceptOrdBuf)
   acceptOrdBuf' <- readMVar acceptOrdBuf 
-  show $ bench "accept time order" (whnf acceptOrdBuf')
+  bench "accept time order" (whnf acceptOrdBuf')
 
-quotePktOrder :: IO String
+quotePktOrder :: Benchmark
 quotePktOrder = do
   pktOrderBuf <- newMVar S.empty
   dump <- readPkts "mdf-kospi200.20110216-0.pcap" (enqueuePktOrd pktOrderBuf)
   pktOrderBuf' <- readMVar pktOrderBuf
-  show $ bench "packet order" (whnf pktOrderBuf')
+  bench "packet order" (whnf pktOrderBuf')
 
-parsePktTime :: String
+parsePktTime :: Benchmark
 parsePktTime =
   let input = ("B6034KR4301F3250500940000679900096000030800095000009400094000023100093000019900092000013400077890009700002340009800001300009900002820010000004150010100000520039700120007000800160009004590011001400170027000709002997"
                :: ByteString)
-  in show $ bench "parser" (whnf $ parsePkt input)
+  in bench "parser" (whnf $ parsePkt input)
--- test/Test.hs
+++ test/Test.hs
@@ -8,7 +8,8 @@ import           Control.Concurrent.MVar
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.Map.Strict as Map (empty)
 import qualified Data.Sequence as S (empty)
-import           Test.Framework (defaultMain)
+import           Test.Tasty (defaultMain)
+import           Test.Tasty.HUnit
 
 main :: IO ()
 main = defaultMain
@@ -17,22 +18,19 @@ main = defaultMain
   , parsePktTest
   ]
 
-quoteAcceptOrdTest :: IO ()
-quoteAcceptOrdTest = do
-  acceptOrdBuf <- newMVar Map.empty
-  readPkts "mdf-kospi200.20110216-0.pcap" (enqueueAcceptOrd acceptOrdBuf)
-  acceptOrdBuf' <- readMVar acceptOrdBuf
-  putStrLn $ show $ acceptOrdBuf'
-
-quotePktOrderTest :: IO ()
-quotePktOrderTest = do
-  pktOrderBuf <- newMVar S.empty
-  readPkts "mdf-kospi200.20110216-0.pcap" (enqueuePktOrd pktOrderBuf)
-  pktOrderBuf' <- readMVar pktOrderBuf
-  putStrLn $ show $ pktOrderBuf'
-
-parsePktTest :: IO ()
-parsePktTest = do
-  let input = ("B6034KR4301F3250500940000679900096000030800095000009400094000023100093000019900092000013400077890009700002340009800001300009900002820010000004150010100000520039700120007000800160009004590011001400170027000709002997"
-               :: ByteString)
-  putStrLn $ show $ parsePkt input
+tests :: TestTree
+tests = testGroup "Tests"
+  [ testCase "Parse one packet" $
+    let input = ("B6034KR4301F3250500940000679900096000030800095000009400094000023100093000019900092000013400077890009700002340009800001300009900002820010000004150010100000520039700120007000800160009004590011001400170027000709002997" :: ByteString)
+    in parsePkt input @?= foo -- | fill in rhs 
+  , testCase "Quotes in order of accept time" $ do
+      acceptOrdBuf <- newMVar Map.empty
+      readPkts "mdf-kospi200.20110216-0.pcap" (enqueueAcceptOrd acceptOrdBuf)
+      acceptOrdBuf' <- readMVar acceptOrdBuf
+      acceptOrdBuf' @?= foo -- | fill in rhs
+  , testCase "Quotes in order of packet time" $ do
+      pktOrderBuf <- newMVar S.empty
+      readPkts "mdf-kospi200.20110216-0.pcap" (enqueuePktOrd pktOrderBuf)
+      pktOrderBuf' <- readMVar pktOrderBuf
+      pktOrderBuf' @?= foo -- | fill in rhs
+  ]
