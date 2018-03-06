1b07da992fafeb26d668e08e628f01959b1a0347
Sophia Gold <sophiagoldnyc@gmail.com>
2018-03-06 01:31:21 -0500

    done overoptimizing :p

--- app/ParseQuote.hs
+++ app/ParseQuote.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import           Capture
-import           Control.Concurrent.Async.Timer
 import           Control.Concurrent.MVar
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Map.Strict as Map
@@ -15,22 +14,10 @@ main = do
     "-r" -> do
       buf <- newMVar Map.empty
       readPkts (args !! 1) (enqueueAcceptOrd buf)
-      let conf = timerConfSetInitDelay 4000 $ timerConfSetInterval 4000 $ defaultTimerConf -- 4s delay
-      withAsyncTimer conf $ \ timer -> do
-        oldBuf <- takeMVar buf
-        let m' = Map.assocs oldBuf
-            m = splitAt (length m' `div ` 4 * 3) m'
-        newBuf <- putMVar buf (Map.fromList $ snd m)
-        sequence_ ((printT . (\(at,(pt,p)) -> (pt,at,p))) <$> fst m)
-        timerWait timer
+      buf <- takeMVar buf
+      sequence_ ((printT . (\(at,(pt,p)) -> (pt,at,p))) <$> Map.assocs buf)
     _    -> do
       buf <- newMVar Map.empty
       readPkts (args !! 0) (enqueuePktOrd buf)
-      let conf = timerConfSetInitDelay 4000 $ timerConfSetInterval 4000 $ defaultTimerConf -- 4s delay
-      withAsyncTimer conf $ \ timer -> do
-        oldBuf <- takeMVar buf
-        let m' = Map.assocs oldBuf
-            m = splitAt (length m' `div ` 4 * 3) m'
-        newBuf <- putMVar buf (Map.fromList $ snd m)
-        sequence_ ((printT . (\(pt,(at,p)) -> (pt,at,p))) <$> fst m)
-        timerWait timer
+      buf <- takeMVar buf
+      sequence_ ((printT . (\(pt,(at,p)) -> (pt,at,p))) <$> Map.assocs buf)
--- test/Bench.hs
+++ test/Bench.hs
@@ -6,16 +6,12 @@ module Main where
 
 import           Capture
 import           Parser
-import           Parser.Types
-import           Control.Concurrent.Async.Timer
 import           Control.Concurrent.MVar
 import           Criterion.Main
 import           Data.ByteString.Char8 (ByteString)
-import           Data.IORef
 import qualified Data.Map.Strict as Map
 import           Data.Text (Text)
-import           Network.Pcap
-import           TextShow (showt, printT)
+import           TextShow (showt)
 
 main :: IO ()
 main = defaultMain
@@ -23,8 +19,6 @@ main = defaultMain
     parsePktTime
   , bench "packet order" (whnfIO quoteAcceptOrdTime)
   , bench "accept time order" (whnfIO quotePktOrder)
-  , bench "incremental packet order" (whnfIO quoteAcceptOrdTime'')
-  , bench "incremental accept time order" (whnfIO quotePktOrder'')
   ]
 
 parsePktTime :: Benchmark
@@ -45,29 +39,3 @@ quotePktOrder = do
   readPkts "test/mdf-kospi200.20110216-0.pcap" (enqueuePktOrd buf)
   buf <- takeMVar buf
   return (showt <$> Map.assocs buf)
-
-quoteAcceptOrdTime'' :: IO ()
-quoteAcceptOrdTime'' = do
-  buf <- newMVar Map.empty
-  readPkts "test/mdf-kospi200.20110216-0.pcap" (enqueueAcceptOrd buf)
-  let conf = timerConfSetInitDelay 4000 $ timerConfSetInterval 4000 $ defaultTimerConf -- 4s delay
-  withAsyncTimer conf $ \ timer -> do
-    oldBuf <- takeMVar buf
-    let m' = Map.assocs oldBuf
-        m = splitAt (length m' `div ` 4 * 3) m'
-    newBuf <- putMVar buf (Map.fromList $ snd m)
-    return (showt <$> (fst m)) 
-    timerWait timer
-      
-quotePktOrder'' :: IO ()
-quotePktOrder'' = do
-  buf <- newMVar Map.empty
-  readPkts "test/mdf-kospi200.20110216-0.pcap" (enqueuePktOrd buf)
-  let conf = timerConfSetInitDelay 4000 $ timerConfSetInterval 4000 $ defaultTimerConf -- 4s delay
-  withAsyncTimer conf $ \ timer -> do
-    oldBuf <- takeMVar buf
-    let m' = Map.assocs oldBuf
-        m = splitAt (length m' `div ` 4 * 3) m'
-    newBuf <- putMVar buf (Map.fromList $ snd m)
-    return (showt <$> (fst m))
-    timerWait timer
